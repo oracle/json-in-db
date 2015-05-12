f5e199d2433de72b5435d75b6be7f3a083c28c8c
markddrake <mark.drake@oracle.com>
2015-05-12 14:37:04 -0700

    Fix InstallDBJSON

--- scripts/installDBJSON.sh
+++ scripts/installDBJSON.sh
@@ -3,8 +3,8 @@ DBAPWD=$2
 ORDSHOME=$3
 if [ -e "$ORDSHOME/ords.war" ]
 then
-  unzip $ORDSHOME/ords.war javax.json-1.0.4.jar
-  unzip $ORDSHOME/ords.war orajsonrest.jar
-  unzip $ORDSHOME/ords.war orasoda.jar
-  sqlplus /nolog @scripts/INSTALL_FROM_JARS.sql $DBA $DBAPWD 
+  unzip -j $ORDSHOME/ords.war WEB-INF/lib/javax.json-1.0.4.jar
+  unzip -j $ORDSHOME/ords.war WEB-INF/lib/orajsonrest.jar
+  unzip -j $ORDSHOME/ords.war WEB-INF/lib/orasoda.jar
+  sqlplus /nolog @scripts/INSTALL_FROM_JAR.sql $DBA $DBAPWD 
 fi

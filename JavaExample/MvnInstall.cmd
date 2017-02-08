1ed86899b2ab285918c53dcd74d35eed08fc59ad
markddrake <mark.drake@oracle.com>
2017-02-08 10:40:54 -0800

    Fix Contents of maven.zip and add MovieTicketingDemo.jar

--- JavaExample/MvnInstall.cmd
+++
@@ -1,5 +0,0 @@
-mvn install:install-file -Dfile=jgeocoder-0.5-jar-with-dependencies.jar -DgroupId=net.sourceforge.jgeocoder -DartifactId=jgeocoder -Dversion=0.5-SNAPSHOT -Dpackaging=jar
-mvn install:install-file -Dfile=ojdbc8.jar -DgroupId=com.oracle.library -DartifactId=oracle-jdbc8 -Dversion=12.2 -Dpackaging=jar
-mvn install:install-file -Dfile=xmlparserv2.jar -DgroupId=com.oracle.library -DartifactId=oracle-xmlparserv2 -Dversion=12.2 -Dpackaging=jar
-mvn install:install-file -Dfile=orajsoda-1.0.3.jar -DgroupId=com.oracle.library -DartifactId=oracle-soda -Dversion=1.0.3 -Dpackaging=jar
-mvn install:install-file -Dfile=MovieTicketingDemo.jar -DgroupId=com.oracle.library -DartifactId=MovieTicketingDemo -Dversion=1.0-SNAPSHOT -Dpackaging=jar

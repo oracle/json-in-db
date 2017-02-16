f686be5ac178e3c35c1fc9133a053189de871f45
markddrake <mark.drake@oracle.com>
2017-02-16 07:43:22 -0800

    Reorganize Maven Component of Java Example 2

---
+++ JavaExample/maven/static/documentation/sampleDocuments.js
@@ -0,0 +1,445 @@
+var theater = {
+  "id": 12,
+  "name": "Orinda Theatre",
+  "location": {
+    "street": "4 Orinda Way",
+    "city": "ORINDA",
+    "zipCode": "94563",
+    "state": "CA",
+    "phoneNumber": null,
+    "geoCoding": {
+      "type": "Point",
+      "coordinates": [
+        -122.19335, 37.886116
+      ]
+    }
+  },
+  "screens": [
+    {
+      "id": 1,
+      "capacity": 106,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 2,
+      "capacity": 113,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 3,
+      "capacity": 85,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 4,
+      "capacity": 99,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 5,
+      "capacity": 101,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 6,
+      "capacity": 121,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 7,
+      "capacity": 110,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 8,
+      "capacity": 103,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 9,
+      "capacity": 128,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }, {
+      "id": 10,
+      "capacity": 121,
+      "features": {
+        "threeD": false,
+        "reserveSeats": false
+      },
+      "ticketPricing": {
+        "adultPrice": 14.95,
+        "childPrice": 9.95,
+        "seniorPrice": 9.95
+      }
+    }
+  ]
+};
+
+var movie = {
+	"id": 153518,
+	"title": "The Angry Birds Movie",
+	"plot": "An island populated entirely by happy, flightless birds or almost entirely. In this paradise, Red, a bird with a temper problem, speedy Chuck, and the volatile Bomb have always been outsiders. But when the island is visited by mysterious green piggies, it∆s up to these unlikely outcasts to figure out what the pigs are up to.",
+	"runtime": 95,
+	"posterURL": "/movieticket/poster/42ADAE57A4F44BDBAC41925E726673DA",
+	"castMember": [
+		{
+			"name": "Jason Sudeikis",
+			"character": "Red (voice)"
+		}, {
+			"name": "Josh Gad",
+			"character": "Chuck (voice)"
+		}, {
+			"name": "Danny McBride",
+			"character": "Bomb (voice)"
+		}, {
+			"name": "Maya Rudolph",
+			"character": "Matilda (voice)"
+		}, {
+			"name": "Bill Hader",
+			"character": "Leonard (voice)"
+		}, {
+			"name": "Peter Dinklage",
+			"character": "Mighty Eagle (voice)"
+		}, {
+			"name": "Sean Penn",
+			"character": "Terence (voice)"
+		}, {
+			"name": "Keegan-Michael Key",
+			"character": "Judge Peckinpah (voice)"
+		}, {
+			"name": "Kate McKinnon",
+			"character": "Stella / Eva the Birthday Mom (voice)"
+		}, {
+			"name": "Tony Hale",
+			"character": "Ross / Cyrus / Mime (voice)"
+		}, {
+			"name": "Hannibal Buress",
+			"character": "Edward the Birthday Dad (voice)"
+		}, {
+			"name": "Ike Barinholtz",
+			"character": "Tiny (voice)"
+		}, {
+			"name": "Tituss Burgess",
+			"character": "Photog (voice)"
+		}, {
+			"name": "Ian Hecox",
+			"character": "Bubbles (voice)"
+		}, {
+			"name": "Anthony Padilla",
+			"character": "Hal (voice)"
+		}, {
+			"name": "Jillian Bell",
+			"character": "Helen the Lunch Mom / Yoga Instructor (voice)"
+		}, {
+			"name": "Billy Eichner",
+			"character": "Chef Pig / Phillip (voice)"
+		}, {
+			"name": "Blake Shelton",
+			"character": "Earl Pig (voice)"
+		}, {
+			"name": "CHARLI XCX",
+			"character": "Willow (voice)"
+		}, {
+			"name": "Ali Wong",
+			"character": "Betty Bird (voice)"
+		}, {
+			"name": "Mckenna Grace",
+			"character": "Ella Bird (voice)"
+		}, {
+			"name": "Ava Acres",
+			"character": "Timothy the Birthday Bird (voice)"
+		}, {
+			"name": "Geoffrey Arend",
+			"character": "Day Care Teacher (voice)"
+		}, {
+			"name": "Alex Borstein",
+			"character": "Sophie Bird / Peggy Bird (voice)"
+		}, {
+			"name": "Max Charles",
+			"character": "Bobby Bird (voice)"
+		}, {
+			"name": "Fergal Reilly",
+			"character": "Foreman Pig (voice)"
+		}, {
+			"name": "Kevin Bigley",
+			"character": "Greg Blue (voice)"
+		}, {
+			"name": "Fred Tatasciore",
+			"character": "Monty Pig (voice)"
+		}, {
+			"name": "Judah Friedlander",
+			"character": "Bill Beakins (voice)"
+		}, {
+			"name": "Danielle Brooks",
+			"character": "Olive Blue / Monica the Crossing Guard (voice)"
+		}, {
+			"name": "Romeo Santos",
+			"character": "Early Bird (voice)"
+		}, {
+			"name": "Cristela Alonzo",
+			"character": "Shirley the Old Lady Bird (voice)"
+		}
+	],
+	"crewMember": [
+		{
+			"name": "Clay Kaytis",
+			"job": "Director"
+		}, {
+			"name": "Fergal Reilly",
+			"job": "Director"
+		}, {
+			"name": "Jon Vitti",
+			"job": "Writer"
+		}, {
+			"name": "John Cohen",
+			"job": "Producer"
+		}, {
+			"name": "Kent Beyda",
+			"job": "Editor"
+		}, {
+			"name": "Kevin Webb",
+			"job": "Animation"
+		}
+	],
+	"releaseDate": "2016-05-20",
+	"certification": "PG",
+	"externalURL": "http://image.tmdb.org/t/p/w185/t2mZzQXjpQxmqtJOPpe8Dr2YpMl.jpg?api_key=ebdef9b4d4764fa65c16039d09eb0eed"
+}
+
+var screening = {
+  "theaterId": 62,
+  "movieId": 382638,
+  "screenId": 1,
+  "startTime": "2016-06-10T20:05:00-07:00",
+  "seatsRemaining": 96,
+  "ticketPricing": {
+    "adultPrice": 14.95,
+    "childPrice": 9.95,
+    "seniorPrice": 9.95
+  }
+};
+
+var ticketSale = {
+  "customerId": 1,
+  "adult": 2,
+  "senior": null,
+  "child": null,
+  "adultPrice": 14.95,
+  "seniorPrice": 9.95,
+  "childPrice": 9.95,
+  "startTime": "2016-06-08T18:35:00-07:00",
+  "theaterId": 12,
+  "screenId": 5,
+  "movieId": 374254,
+  "purchaseDate": "2016-06-08T13:59:59-07:00"
+};
+
+var sampleConfig = {
+  "hostname" : "localhost",
+  "port" : 8080,
+  "path" : "/ords/movies/soda/latest",
+  "username" : "MovieTicketing",
+  "password" : "MovieTicketing"
+}
+
+var sampleDataSources = {
+  "useProxy": false,
+  "proxy": {
+    "hostname": null,
+    "port": null
+  },
+  "tmdb": {
+    "protocol": "http",
+    "hostname": "api.themoviedb.org",
+    "port": 80,
+    "apiPath": "/3",
+    "apiKey": "YOUR_TMDB_KEY_GOES_HERE",
+    "searchCriteria": {
+      "language": "en",
+      "country": "US",
+      "certification": "PG-13",
+      "popularity": 2,
+      "releaseDates": {
+        "start": "2013-01-01",
+        "end": "2016-06-30"
+      },
+      "movieLimit": 1024
+    }
+  },
+  "fandango": {
+    "protocol": "http",
+    "hostname": "www.fandango.com",
+    "port": 80,
+    "path": "/rss/moviesnearme_",
+    "searchCriteria": {
+      "zipCode": "94501"
+    }
+  },
+  "usCensus": {
+    "protocol": "http",
+    "hostname": "geocoding.geo.census.gov",
+    "port": 80,
+    "path": "/geocoder/locations/onelineaddress"
+  },
+  "google": {
+    "apiKey": "YOUR_GOOGLE_KEY_GOES_HERE"
+  }
+}
+
+function escapeHtml(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+}
+
+var theaterServiceHTML = 
+`<div class="tab-pane active" id="tab_TheaterList">
+	<div id="TheaterList" class="panel panel-default" ng-controller="theatersCtrl">
+  		<div class="panel-heading">
+  			<h3 class="panel-title" style="line-height:35px;">Theater Information
+	    		<span class="pull-right">
+	  	    		<button class="btn btn-success btn-med" id="btn_TheaterSearch" type="button" onclick="showTheaterSearch()">
+				 		<span class="glyphicon glyphicon-search"></span>
+					</button>
+			    </span>
+			</h3>
+    	</div>
+      	<div class="panel-body" style="height:65vh; overflow: auto;">
+      		<table class="table table-fixed">
+      			<thead>
+      				<tr>
+      					<th>Name</th>
+      					<th>Address</th>
+      				  <th></th>
+      				</tr>
+      			</thead>
+      			<tbody>
+      				<tr ng-repeat="theater in theaterService.theaters">
+     					<td>{{theater.value.name}}</td>
+     					<td>{{theater.value.location.street}} {{theater.value.location.city}} {{theater.value.location.state}} {{theater.value.location.zipCode}}</td>
+     					<td>
+      	    				<button id="btn_MoviesByTheater" type="button" class="btn btn-default btn-success" ng-click="theaterService.getMoviesByTheater(theater.id)">Movies</button>
+     				    </td>
+     				</tr>
+     			</tbody>
+     		</table>
+     	</div>
+		<div>
+			<br>
+		</div>
+	    <div class="container row" id="log_TheaterList">
+        	<table>
+	      		<tbody>
+ 		      		<tr>
+		      			<td style="padding-right:1em; width:100px; vertical-align:top;">
+		      				<div class="btn-group" role="group">
+  						    	<button type="button" class="btn btn-default btn-med" ng-disabled="true" >
+  							    	<span class="glyphicon glyphicon-arrow-up"></span>
+  						    	</button>
+  						    	<button type="button" class="btn btn-default btn-med" ng-disabled="true">
+  							    	<span class="glyphicon glyphicon-arrow-down"></span>
+  						    	</button>
+  						    </div>
+  					  	</td>
+					 	<td>
+					 		<table>
+					 			<tbody>
+					 				<tr>
+					 					<td>
+	    			  	       		  		<span style="margin-right:1em;"><strong>Method:</strong> {{theaterService.logRecord[0].value.method}}</span>
+  	  	    			      	 			<span style="margin-right:1em;"><strong>URL:</strong> {{theaterService.logRecord[0].value.url}}</span>
+    	  	    				 		<td>
+      	  	     	 				</tr>
+        	  		 				<tr ng-if="theaterService.logRecord[0].value.request.body.length > 0">
+      	 								<td><strong>Body:</strong> {{theaterService.logRecord[0].value.request.body}}</td>
+      	 							</tr>
+	          		 				<tr>
+      				 					<td><strong>Status:</strong> {{theaterService.logRecord[0].value.response.statusCode}} [{{theaterService.logRecord[0].value.response.statusText}}] <strong>Elapsed Time:</strong> {{theaterService.logRecord[0].value.elapsedTime}}ms.</td>
+      	 							</tr>
+      	 						</tbody>
+      	 					</table>
+      	 				</td>
+      	 			</tr>	
+      	 		</tbody>
+      	 	</table>
+      	 </div>
+    </div>
+</div>`;
+
+function renderSampleDocuments() {
+  
+  jPP.printJson(document.getElementById("sampleTheater"),null,theater);
+  jPP.printJson(document.getElementById("sampleMovie"),null,movie);
+  jPP.printJson(document.getElementById("sampleScreening"),null,screening);
+  jPP.printJson(document.getElementById("sampleTicketSale"),null,ticketSale);
+  jPP.printJson(document.getElementById("sampleConfig"),null,sampleConfig);
+  jPP.printJson(document.getElementById("sampleDataSources"),null,sampleDataSources);
+  document.getElementById('theaterServiceHTML').innerHTML=hljs.highlight('html', theaterServiceHTML).value;
+}

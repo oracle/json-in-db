c4c2af62831d3bd4d0b1bfa2f942169588be8265
kzhou312 <35820072+kzhou312@users.noreply.github.com>
2019-03-26 11:56:07 -0700

    Update resetLab.sql

--- SODA4REST-HOL/hol/clone/resetLab.sql
+++ SODA4REST-HOL/hol/clone/resetLab.sql
@@ -5,4 +5,8 @@ call XDB.DBMS_SODA_ADMIN.drop_Collection('MyCollection')
 /
 call XDB.DBMS_SODA_ADMIN.drop_Collection('MyCustomCollection')
 /
+exec ords.enable_schema;
+/
+exec ords.delete_privilege_mapping('oracle.soda.privilege.developer', '/soda/*');
+/
 quit

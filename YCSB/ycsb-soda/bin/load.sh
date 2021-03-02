f80cd69352a48ae2c6bcdf6427d16c36c21deb31
SrinivasKareenhalli <srinivas.kareenhalli@oracle.com>
2021-03-02 10:23:40 -0800

    Added load and run scripts

---
+++ YCSB/ycsb-soda/bin/load.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source bin/env
+
+bin/ycsb load soda -cp $CLASSPATH -P ./workloads/workloadc -jvm-args "-Xmx32g -Xms32g -Dsun.net.inetaddr.ttl=0" -s -threads 20 -p soda.url=jdbc:oracle:thin:@<db_service_name>?TNS_ADMIN=<tns and wallet dirname>-p soda.user=<dbuser> -p soda.password=<db pasword> -p table=<collection name> -p recordcount=4096000 -p maxexecutiontime=600  -p operationcount=1000000000
+ 
--- YCSB/ycsb-soda/bin/run.sh
+++ YCSB/ycsb-soda/bin/run.sh
@@ -2,4 +2,5 @@
 
 source bin/env
 
-bin/ycsb run soda -cp $CLASSPATH -P ./workloads/workloadc -jvm-args "-Xmx32g -Xms32g -Dsun.net.inetaddr.ttl=0" -s -threads 20 -p soda.url=jdbc:oracle:thin:@db202012241234_tp?TNS_ADMIN=/home/opc/skareenh/tns_atphash19_clone -p soda.user=json -p soda.password=OracleSoda1! -p table=TESTTABLE -p recordcount=4096 -p maxexecutiontime=60  -p operationcount=1000000000
+bin/ycsb run soda -cp $CLASSPATH -P ./workloads/workloadb -jvm-args "-Xmx32g -Xms32g -Dsun.net.inetaddr.ttl=0" -s -threads 20 -p soda.url=jdbc:oracle:thin:@<db_service_name>?TNS_ADMIN=<tns and wallet dirname>-p soda.user=<dbuser> -p soda.password=<db pasword> -p table=<collection name> -p recordcount=4096000 -p maxexecutiontime=600  -p operationcount=1000000000
+ 

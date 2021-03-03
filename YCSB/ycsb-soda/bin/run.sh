1c7c97744a6d30a1999f7a621a08325259d0bc9a
SrinivasKareenhalli <srinivas.kareenhalli@oracle.com>
2021-03-03 05:00:47 -0800

    Modified run scripts

--- YCSB/ycsb-soda/bin/run.sh
+++ YCSB/ycsb-soda/bin/run.sh
@@ -2,5 +2,5 @@
 
 source bin/env
 
-bin/ycsb run soda -cp $CLASSPATH -P ./workloads/workloadb -jvm-args "-Xmx32g -Xms32g -Dsun.net.inetaddr.ttl=0" -s -threads 20 -p soda.url=jdbc:oracle:thin:@<db_service_name>?TNS_ADMIN=<tns and wallet dirname>-p soda.user=<dbuser> -p soda.password=<db pasword> -p table=<collection name> -p recordcount=4096000 -p maxexecutiontime=600  -p operationcount=1000000000
+bin/ycsb run soda -cp $CLASSPATH -P ./workloads/workloadb -jvm-args "-Xmx32g -Xms32g -Dsun.net.inetaddr.ttl=0" -s -threads <threads> -p soda.url=jdbc:oracle:thin:@<db_service_name>?TNS_ADMIN=<tns and wallet dirname>-p soda.user=<dbuser> -p soda.password=<db pasword> -p table=<collection name> -p recordcount=4096000 -p maxexecutiontime=600  -p operationcount=1000000000
  
